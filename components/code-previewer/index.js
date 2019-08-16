024d8f8305b25ac865921f6334ff2af4f3388a93
张威 <zw900808@gmail.com>
2019-08-16 15:12:51 +0800

    发布新版本

--- components/code-previewer/index.js
+++ components/code-previewer/index.js
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 
 class CodePreviewer extends Component {
   render() {
-    <div className="code-preview-container">
-      <div className="cp-component">
-        <div className="component-header">header</div>
-        <div className="component-content">content</div>
-        <div className="component-footer">footer</div>
+    return (
+      <div className="code-preview-container">
+        <div className="cp-component">
+          <div className="component-header">header</div>
+          <div className="component-content">content</div>
+          <div className="component-footer">footer</div>
+        </div>
+        <div className="cp-code">
+          <div className="code-header">code header</div>
+          <div className="code-tabs">code tabs</div>
+          <div className="code-content">code content</div>
+        </div>
       </div>
-      <div className="cp-code">
-        <div className="code-header">code header</div>
-        <div className="code-tabs">code tabs</div>
-        <div className="code-content">code content</div>
-      </div>
-    </div>;
+    );
   }
 }
 
