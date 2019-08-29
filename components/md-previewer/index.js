601af70faae37cdeaf7324942e7b5005d127684e
张威 <zw900808@gmail.com>
2019-08-29 09:54:36 +0800

    修改 markdown 样式

--- components/md-previewer/index.js
+++ components/md-previewer/index.js
@@ -3,15 +3,23 @@ import PropTypes from "prop-types";
 import cls from "classnames";
 import ReactMarkdown from "react-markdown";
 import CodeBlock from "../code-block";
+const htmlParser = require("react-markdown/plugins/html-parser");
 
 class MarkdownPreviewer extends PureComponent {
   render() {
     const { md } = this.props;
+    const parseHtml = htmlParser({
+      isValidNode: node => node.type !== "script",
+      processingInstructions: [
+        /* ... */
+      ]
+    });
     return (
       <div className="markdown-previewer-container">
         <ReactMarkdown
           source={md}
           escapeHtml={false}
+          // astPlugins={[parseHtml]}
           renderers={{
             code: CodeBlock
           }}
--- src/md/install.md
+++ src/md/install.md
@@ -6,6 +6,11 @@
 yarn add react-code-previewer
 ```
 
+<span style="color:red; font-family:Helvetica Neue, Helvetica, Arial, sans-serif; font-size:2em;">sdfsdf
+</span>
+
+<h1>ddd</h1>
+
 ## Usage
 
 ```jsx
